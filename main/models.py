d108062193bcc5caa82a8cdc31e1abfbd916d7ab
ibnurisk11 <ibnuriski28@gmail.com>
2025-07-11 00:24:36 +0700

    update

--- main/models.py
+++ main/models.py
@@ -14,7 +14,7 @@ class InventoryItem(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     location = models.CharField(max_length=100)
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='inventory_images/', null=True, blank=True)  # <-- Tambahkan ini
+    image = models.ImageField(upload_to='inventory_images/', null=True, blank=True)
     added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
